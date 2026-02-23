6337285033f6ef193e8fe9baffe0e417e0eabdbc
jae <wod6983@gmail.com>
2026-02-23 12:02:51 +0900

    삭제 재귀함수 구현 및 기능 테스트 진행
    - 삭제 재귀 함수 구현
    - SearchMinValue 함수 구현 (최소값 검색 함수)
    - 삭제 기능 테스트 (Main.cpp 에서 진행)

---
+++ 11.BST/Container/BinarySearchTree.h
@@ -0,0 +1,204 @@
+﻿#pragma once
+
+#include "Node.h"
+
+// 이진 탐색 트리 클래스.
+template<typename T>
+class BinarySearchTree
+{
+public:
+    BinarySearchTree()
+    {
+    }
+
+    ~BinarySearchTree()
+    {
+        // Todo: 트리 제거 함수 구현 후 호출.
+    }
+
+    // 삽입.
+    // 규칙
+    // 0.   중복된 값 허용 안함.
+    // 1.   루트 노드부터 비교 시작.
+    // 1-1. 루트가 null이면 루트 생성.
+    // 2.   추가 할 값이 작으면 왼쪽에 저장.
+    // 3.   추가 할 값이 크면 오른쪽에 저장.
+    bool InsertNode(const T& newData)
+    {
+        // 중복 여부 확인.
+        Node<T>* outNode = nullptr;
+        if (SearchNode(newData, outNode))
+        {
+            return false;
+        }
+
+        if (!root)
+        {
+            root = new Node<T>(newData);
+            return true;
+        }
+        
+        // 2,3을 처리하기 위해 재귀 함수 호출.
+        root = InsertNodeRecursive(root, nullptr, newData);
+        return true;
+    }
+
+    // 삭제.
+    bool DeleteNode(const T& deleteData)
+    {
+        // Todo: 재귀 삭제 함수 구현 후 호출.
+        return DeleteNodeRecursive(root, deleteData, root);
+    }
+
+    // 검색.
+    bool SearchNode(const T& data, Node<T>*& outNode)
+    {
+        // 검색 재귀함수 구현 후 호출.
+        return SearchNodeRecursive(root, data, outNode);
+    }
+    // 순회.
+
+private:
+    // 삽입 재귀 함수.
+    Node<T>* InsertNodeRecursive(Node<T>* node, Node<T>* parent, const T& newData)
+    {
+        if (!node)
+        {
+            return new Node<T>(newData, parent);
+        }
+
+        // 추가하려는 값이 비교 노드 보다 작으면 왼쪽 하위 트리로 탐색 진행
+        if (node->data > newData)
+        {
+            node->left = InsertNodeRecursive(node->left, node, newData);
+        }
+        // 추가하려는 값이 비교 노드 보다 크면 오른쪽 하위 트리로 탐색 진행
+        else
+        {
+            node->right= InsertNodeRecursive(node->right, node, newData);
+        }
+        // 트리 구조 유지를 위해 반환.
+        return node;
+    }
+
+    // 검색 재귀 함수.
+    bool SearchNodeRecursive(Node<T>* node, const T& data, Node<T>*& outNode)
+    {
+        // 검색 실패.
+        if (!node)
+        {
+            outNode = nullptr;
+            return false;
+        }
+
+        // 찾았는지 확인.
+        if (node->data == data)
+        {
+            outNode = node;
+            return true;
+        }
+
+        // 작은 경우 왼쪽으로.
+        if (node->data > data)
+        {
+            return SearchNodeRecursive(node->left, data, outNode);
+        }
+        // 큰 경우 오른쪽으로
+        else
+        {
+            return SearchNodeRecursive(node->right, data, outNode);
+        }
+    }
+    
+    // 삭제 재귀 함수.
+    bool DeleteNodeRecursive(Node<T>* node, const T& deleteData, Node<T>*& outNode)
+    {
+        // 노드가 null인 경우는 삭제할 노드를 찾지 못한 경우.
+        // 삭제 실패.
+        if (!node)
+        {
+            outNode = nullptr;
+            return false;
+        }
+
+        // 비교 값이 작은 경우 왼쪽으로.
+        if (node->data > deleteData)
+        {
+            return DeleteNodeRecursive(node->left, deleteData, node->left);
+        }
+        // 비교 값이 큰 경우 오른쪽으로.
+        else if (node->data < deleteData)
+        {
+            return DeleteNodeRecursive(node->right, deleteData, node->right);
+        }
+        // 삭제 노드 찾은 경우 처리.
+        else
+        {
+            // 경우의 수 1 - 자식이 없는 경우(left,right 모두 null인 경우).
+            if (!node->left && !node->right)
+            {
+                delete node;
+                outNode = nullptr;
+                return true;
+            }
+            // 경우의 수 2 - 자식이  둘다 있는 경우 -> 왼쪽 트리의 가장 큰것 OR 오른쪽 트리의 가장 작은 것(아무거나 가능)
+            if (node->left && node->right)
+            {
+                // Way 1 - 왼쪽 트리 중 가장 큰 값으로 대체
+                // Way 2 - 오른쪽 트리의 가장 작은 값으로 대체
+                // 하위 노드에서 최소값 찾는 함수 호출.
+                node->data = SearchMinValue(node->right)->data;
+
+                DeleteNodeRecursive(node->right, node->data, node->right);
+                return true;
+            }
+
+            // 한 쪽만 있는 경우
+            // 오른쪽 자식만 있는 경우
+            if (!node->left)
+            {
+                Node<T>* right = node->right;
+
+                // 오른쪽 자손의 부모를 삭제할 노드의 부모로 설정.
+                right->parent = node->parent;
+
+                delete node;
+
+                outNode = right;
+                return true;
+            }
+            // 왼쪽 자식만 있는 경우.
+            if (!node->right)
+            {
+                Node<T>* left = node->left;
+
+                // 왼쪽 자손의 부모를 삭제할 노드의 부모로 설정.
+                left->parent = node->parent;
+
+                delete node;
+
+                outNode = left;
+                return true;
+            }
+        }
+        outNode = nullptr;
+        return false;
+    }
+
+    // 최솟값 검색함수
+    // node: 검색을 시작하는 노드
+    Node<T>* SearchMinValue(Node<T>* node)
+    {
+        while (node->left)
+        {
+            node = node->left;
+        }
+        return node;
+    }
+
+    // 파괴 함수.
+
+private:
+    // 루트 노드.
+    Node<T>* root = nullptr;
+};
